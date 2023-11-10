a04b4932653f9909b76bb142f70b8ec545336076
RobertoScarci <robertoscarci.02@gmail.com>
2023-11-10 12:06:24 +0100

    completamento con gli elementi if e else

--- snack-1/js/script.js
+++ snack-1/js/script.js
@@ -1,2 +1,12 @@
-let firstAge= prompt('Scrivi la tua età')
-let secondAge= prompt('Scrivi gli anni del tuo migliore amico')
+
+
+const firstAge= prompt('Scrivi la tua età')
+const secondAge= prompt('Scrivi gli anni del tuo migliore amico')
+
+if ( firstAge > secondAge){
+    console.log ('Sei più grande del tuo amico')
+} else if ( secondAge > firstAge ){
+    console.log ('Il tuo amico è più grande di te')
+} else {
+    console.log ('Avete la stessa età')
+} 
