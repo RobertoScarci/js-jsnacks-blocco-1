dc6d545da6a4b5f4c738356b048c84ca98a96a5e
RobertoScarci <robertoscarci.02@gmail.com>
2023-11-14 12:39:27 +0100

    creazione dello script.js inserendo i numeri di due in due

---
+++ snack-3/js/script.js
@@ -0,0 +1,12 @@
+const  listElement = document.querySelector('div.list')
+
+// creo la sezione for per inserire i primi 6 numeri
+
+for ( let i = 0; i <= 20; i = i + 2){
+    const cardElement = document.createElement('h2')
+    cardElement.innerHTML = i
+
+    listElement.appendChild(cardElement)
+
+    console.log(i)
+}
