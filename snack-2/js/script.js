161d89dd22205e78883c5cacc568a2d8df263bbb
RobertoScarci <robertoscarci.02@gmail.com>
2023-11-14 12:27:10 +0100

    aggiunta delle classi per ogni numero al riconoscimento se pari o dispari

--- snack-2/js/script.js
+++ snack-2/js/script.js
@@ -1,13 +1,19 @@
-const  listElement = document.querySelector('ul.list')
+const  listElement = document.querySelector('div.list')
 
 // creo la sezione for per inserire i primi 6 numeri
 
 for ( let i = 1; i < 7; i++){
-    const cardElement = document.createElement('li')
+    const cardElement = document.createElement('h2')
     cardElement.className = 'number'
     cardElement.innerHTML = i
 
     listElement.appendChild(cardElement)
 
     console.log(cardElement)
+
+    if( i % 2 == 0){
+        cardElement.className = 'text-green'
+    } else {
+        cardElement.className = 'text-dark'
+    }
 }
