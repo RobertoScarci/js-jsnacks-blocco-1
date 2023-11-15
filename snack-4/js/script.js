39e5a67032d820c0359e881aa75ad361ffaa04e9
RobertoScarci <robertoscarci.02@gmail.com>
2023-11-15 12:28:15 +0100

    modifica dello snack

--- snack-4/js/script.js
+++ snack-4/js/script.js
@@ -8,9 +8,6 @@ for(let i = 0; i <= 100; i++){
     } else if (i % 8 == 0){
         array.push(i)
     }
-    console.log(array[i])
 }
 
-for(let i = 0; i < array.length; i++){
-    console.log(array[i])
-}
+console.log(array);
